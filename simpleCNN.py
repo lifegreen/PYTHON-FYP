19f8b1bd919be1085e98b78acfdfcc318197d706
Mark <lifegreen.m@gmail.com>
2020-05-06 18:02:57 +0100

    First 80%

--- simpleCNN.py
+++ simpleCNN.py
@@ -1,8 +1,9 @@
 print("At least the script starts...")
+import gumpy
 import numpy as np
 import keras
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D, Dense, Flatten
+from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D, Dense, Flatten, BatchNormalization
 from keras.utils import to_categorical
 
 from scipy.io import loadmat
@@ -15,13 +16,13 @@ from datetime import datetime
 
 def isListEmpty(inList):
 # Checks if a list consists only of Empty nested lists
-        if isinstance(inList, list): # Is a list (returns false if empty)???
+        if isinstance(inList, list): # if list (returns false if empty)
             return all(map(isListEmpty, inList))
         return False # Not a list
 
 class NinaproDB:
-    winSize = 25
-    overlap = 0
+    winSize = 256
+    step = 64
 
     def __init__(self):
         ## PLZ PLZ PLZ FIX ##
@@ -39,8 +40,8 @@ class NinaproDB:
                 if not isListEmpty(sub):
                     subCount += 1
                     for rep in sub:
-                        for ges in range(len(rep)):
-                            gesCount[ges] += len(rep[ges])
+                        for ges, num in zip(rep, range(len(rep))):
+                            gesCount[num] += len(ges)
 
         string = "%d %s \n" % (subCount, 'subject' if subCount==1 else 'subjects')
         string += "%d windows total\n" % sum(gesCount)
@@ -63,23 +64,21 @@ class NinaproDB:
         dataset = loadmat(file)
         signal = dataset['emg']
         labels = dataset['restimulus']
-        exe    = dataset['exercise']
-        sub    = dataset['subject']
+        exe    = dataset['exercise'].item() - 1
+        sub    = dataset['subject'].item() - 1
         rep    = dataset['rerepetition']
 
         # Divide signal into time windows
         # length = (len(signal) // winSize) * winSize
-
-        step = self.winSize - self.overlap
         # self.windows = [signal[i : i + self.winSize] for i in range(0, len(signal), step)]
         # if (self.windows[-1].shape != self.windows[1].shape) :
         #     del self.windows[-1]
 
-        for start in range(0, len(signal) - self.winSize, step):
+        for start in range(0, len(signal) - self.winSize, self.step):
             end = start + self.winSize
             if labels[start] == labels[end-1]:
 #PLS FIX#
-                self.Data[exe[0][0]-1][sub[0][0]-1][rep[start][0]-1][labels[start][0]].append(signal[start:end])
+                self.Data[exe][sub][rep[start].item() - 1][labels[start].item()].append(signal[start:end])
 # [0] are to turn a for some reason nested list into a scalar
 # -1  is for converting to 0 indexing
 
@@ -128,6 +127,10 @@ print(DB)
 [TrainX, TrainY , ValidX, ValidY] = DB.prepareData(0.3)
 print(list(map(len, [TrainX, TrainY , ValidX, ValidY])))
 
+# results, clf = gumpy.classify('SVM', X_train, Y_train, X_test, Y_test)
+
+# print(results)
+
 TrainX = [np.expand_dims(x, axis=2) for x in TrainX]
 TrainX = np.stack(TrainX, axis=0 )
 
@@ -170,8 +173,10 @@ model = Sequential()
 # model.add(Dense(num_classes, activation='softmax'))
 
 ## PAPER 3 ##
-model.add(Conv2D(32, kernel_size=5, activation='relu', input_shape=(25,10,1), padding='same'))
-model.add(Conv2D(32, kernel_size=3, activation='relu', input_shape=(25,10,1), padding='same'))
+model.add(Conv2D(32, kernel_size=5, activation='relu', input_shape=(256,10,1), padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(32, kernel_size=3, activation='relu', padding='same'))
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
 
 model.add(Conv2D(64, kernel_size=3, activation='relu', padding='same'))
